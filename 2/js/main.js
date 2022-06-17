c3902bfd172f13fab86e5e330c464c76b1b180a3
Keks <keks@htmlacademy.ru>
2022-06-17 23:46:47 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/main.js
@@ -0,0 +1,9 @@
+const getRandomInt = (arg1, arg2) => {
+    const fromNumber = Math.min(arg1, arg2);
+    const toNumber = Math.max(arg1, arg2);
+    return Math.floor(fromNumber + Math.random() * (toNumber + 1 - fromNumber));
+};
+getRandomInt(1, 3);
+
+const getStringLength = (string, maxlength) => string.length <= maxlength;
+getStringLength('Hello world', 10);
