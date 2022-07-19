1b9163393fe12d6075190f550e14a54df4784bf1
Romka <prostoonado@gmail.com>
2022-07-19 18:30:40 +0300

    Отобразил фотографии других пользователей

---
+++ js/renderingArt.js
@@ -0,0 +1,18 @@
+import { SIMILAR_OBJECTS } from './data';
+
+const container = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const miniatures = SIMILAR_OBJECTS(25);
+
+const fragment = document.createDocumentFragment();
+
+miniatures.forEach(({ url, likes, comments }) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('[src]').textContent = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments;
+  fragment.appendChild(pictureElement);
+});
+
+container.appendChild(fragment);
