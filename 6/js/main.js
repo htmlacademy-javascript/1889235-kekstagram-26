faaff2101a2990cbeaf97f3efd51059fec9e8f93
Keks <keks@htmlacademy.ru>
2022-07-20 16:40:22 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,6 +1,5 @@
-import { SIMILAR_OBJECTS } from './data';
-import { checkStringLength } from './utils';
+import { SIMILAR_OBJECTS } from './data.js';
+import { checkStringLength } from './utils.js';
 
 SIMILAR_OBJECTS(25);
 checkStringLength('Hello world', 10);
-
