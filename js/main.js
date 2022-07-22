4b179f8f13c779b4e927ef5dfde559c0e66652cd
Romka <prostoonado@gmail.com>
2022-07-22 18:42:15 +0300

    Подключение модуля

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import { SIMILAR_OBJECTS } from './data.js';
 import { checkStringLength } from './utils.js';
 import { renderingPhoto } from './rendering-photo.js';
+import './popup-image.js';
 
 SIMILAR_OBJECTS(25);
 checkStringLength('Hello world', 10);
