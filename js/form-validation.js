796da8795ef6f7623cebad97870dfc65669fb022
Romka <prostoonado@gmail.com>
2022-07-24 20:40:33 +0300

    Начало некст задания

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,4 +1,27 @@
-const form = document.querySelector('#upload-select-image');
+// export const formValidate = () => {
+//   const form = document.querySelector('#upload-select-image');
 
-new Pristine(form);
+//   const pristine = new Pristine(form, {
+//     classTo: 'img-upload__field-wrapper',
+//     errorClass: 'form__item--invalid',
+//     successClass: 'form__item--valid',
+//     errorTextParent: 'img-upload__field-wrapper',
+//     errorTextTag: 'span',
+//     errorTextClass: 'form__error'
+//   }, false);
+//   const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
 
+//   const validateHashtags = (value) => re.test(value);
+//   const validateComments = (value) => value < 140;
+
+//   pristine.addValidator(form.querySelector('.text__hashtags'), validateHashtags, 'не правильный хештег');
+
+//   pristine.addValidator(form.querySelector('.text__description'), validateComments, 'не больше 140 символов');
+
+//   const onSubmit = (evt) => {
+//     evt.preventDefault();
+//     pristine.validate();
+//   };
+
+//   form.addEventListener('submit', onSubmit);
+// };
