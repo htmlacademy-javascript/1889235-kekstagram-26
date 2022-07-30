64b65b9c12fbf544ba097f39c62978514dad353b
Romka <prostoonado@gmail.com>
2022-07-31 00:12:15 +0300

    убрал работу esс при фокусе

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,7 +1,6 @@
 import { isEscapeKey } from './utils.js';
-
+const form = document.querySelector('#upload-select-image');
 export const formValidate = () => {
-  const form = document.querySelector('#upload-select-image');
 
   const pristine = new Pristine(form, {
     classTo: 'img-upload__field-wrapper',
@@ -30,6 +29,7 @@ export const formValidate = () => {
   form.addEventListener('submit', onSubmit);
 };
 
+
 const onPopupEsc = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
@@ -40,25 +40,37 @@ const changeEditContainer = document.querySelector('.img-upload__overlay');
 const formChangeOpen = document.querySelector('#upload-file');
 const formChangeClose = changeEditContainer.querySelector('#upload-cancel');
 
+let areInputsFocused = false;
+
 function formEditOpen() {
   changeEditContainer.classList.remove('hidden');
 
   document.addEventListener('keydown', onPopupEsc);
 
   document.querySelector('body').classList.add('modal-open');
+
 }
 
 function formEditClose() {
+  if (areInputsFocused === true) {
+    return;
+  }
   changeEditContainer.classList.add('hidden');
-
   document.removeEventListener('keydown', onPopupEsc);
-
   document.querySelector('body').classList.remove('modal-open');
 }
-
 
 formChangeOpen.addEventListener('change', formEditOpen);
 
 formChangeClose.addEventListener('click', formEditClose);
 
-
+document
+  .querySelectorAll('.required-input')
+  .forEach((input) => {
+    input.addEventListener('focus', () => {
+      areInputsFocused = true;
+    });
+    input.addEventListener('blur', () => {
+      areInputsFocused = false;
+    });
+  });
